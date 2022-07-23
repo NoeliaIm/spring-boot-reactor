6ad52088674b724c8599d33071b035a931d632ce
Noelia <tuppersex.ni@gmail.com>
2022-07-23 18:03:11 +0200

    Operadores

---
+++ src/main/java/com/noeliaiglesias/springbootreactor/app/model/Usuario.java
@@ -0,0 +1,14 @@
+package com.noeliaiglesias.springbootreactor.app.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Usuario {
+
+    private String nombre;
+    private String apellido;
+}
