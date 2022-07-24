7bf9a9bc675690417072ec8075ea863b6fc9b984
Noelia <tuppersex.ni@gmail.com>
2022-07-24 13:26:51 +0200

    Flux desde iterables

--- src/main/java/com/noeliaiglesias/springbootreactor/app/SpringBootReactorApplication.java
+++ src/main/java/com/noeliaiglesias/springbootreactor/app/SpringBootReactorApplication.java
@@ -6,7 +6,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import reactor.core.publisher.Flux;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
+
 
 @SpringBootApplication
 public class SpringBootReactorApplication implements CommandLineRunner {
@@ -19,17 +22,36 @@ public class SpringBootReactorApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-        Flux<String> nombres = Flux.just("Noelia", "Pedro", "Juan");
-        nombres.map(el -> new Usuario(el, null))
-                .doOnNext(user -> {
-                    if (user == null) {
-                        throw new RuntimeException("El nombre no puede ser vacío");
-                    }
-                    System.out.println("Emitiendo: " + user);
-                })
-                .map(user -> user.getNombre().toUpperCase());
+        List<String> usuariosList = new ArrayList<>();
+        usuariosList.add("Noelia Iglesias");
+        usuariosList.add("Pedro Romero");
+        usuariosList.add("Juan González");
+        usuariosList.add("María López");
+        usuariosList.add("Pepe Viyuela");
+        usuariosList.add("Pepe Navarro");
 
-        Flux<Usuario> nombres2 = Flux.just("Noelia Iglesias", "Pedro Romero ", "Juan González", "Pepe Viyuela", "Pepe Navarro")
+        Flux<String> nombres = Flux.fromIterable(usuariosList); /*Flux.just("Andres Guzman" , "Pedro Fulano" , "Maria Fulana", "Diego Sultano", "Juan Mengano", "Bruce Lee", "Bruce Willis");*/
+
+        Flux<Usuario> usuarios = nombres.map(nombre -> new Usuario(nombre.split(" ")[0].toUpperCase(), nombre.split(" ")[1].toUpperCase()))
+                .filter(usuario -> usuario.getNombre().equalsIgnoreCase("pepe"))
+                .doOnNext(usuario -> {
+                    if (usuario == null) {
+                        throw new RuntimeException("Nombres no pueden ser vacíos");
+                    }
+
+                    System.out.println(usuario.getNombre().concat(" ").concat(usuario.getApellido()));
+                })
+                .map(usuario -> {
+                    String nombre = usuario.getNombre().toLowerCase();
+                    usuario.setNombre(nombre);
+                    return usuario;
+                });
+
+        usuarios.subscribe(e -> log.info(e.toString()),
+                           error -> log.severe(error.getMessage()),
+                           () -> log.info("Ha finalizado la ejecución del observable con éxito!"));
+
+        Flux<Usuario> users = Flux.just("Noelia Iglesias", "Pedro Romero ", "Juan González", "Pepe Viyuela", "Pepe Navarro")
                 .map(el -> new Usuario(el.split(" ")[0], el.split(" ")[1]))
                 .filter(user -> user.getNombre().equals("Pepe"))
                 .doOnNext(user -> {
@@ -45,7 +67,7 @@ public class SpringBootReactorApplication implements CommandLineRunner {
 
         nombres.subscribe(log::info, error -> log.severe("Error: " + error.getMessage()), () -> log.info("Terminó nombres"));
 
-        nombres2.subscribe(e -> log.info(e.getNombre()), error -> log.severe("Error: " + error.getMessage()), () -> log.info("Terminó nombres2"));
+        users.subscribe(e -> log.info(e.getNombre()), error -> log.severe("Error: " + error.getMessage()), () -> log.info("Terminó nombres2"));
 //        Los fluxes son inmutables, es decir, los originales no se modifican.
 //        Los fluxes son observables, es decir, se pueden suscribir a ellos.
 //        Los fluxes son flujos de datos, es decir, se pueden emitir datos.
@@ -53,6 +75,6 @@ public class SpringBootReactorApplication implements CommandLineRunner {
 //        Los fluxes son flujos de acciones, es decir, se pueden ejecutar acciones.
 
         nombres.subscribe(System.out::println);
-        nombres2.subscribe(System.out::println);
+        users.subscribe(System.out::println);
     }
 }
